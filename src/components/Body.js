e674367eab91cc7a8487febf374752842acd3233
Likhitha J <likhithajlikhi@gmail.com>
2025-04-26 09:55:29 +0530

    api changed

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,31 +1,37 @@
+
+
+
 import { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
-import resList from "../../utils/mockData";
-
 
 
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants]  = useState([]);
 
-  const [listOfRestaurants, setListOfRestaurants]  = useState(resList);
-
+  
+  
     useEffect(() => {
+      // getRestaurantList();
       fetchData();
     },[]);
-  
-  const fetchData = async () => {
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.8933622&lng=77.6258081&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
- 
-    const json = await data.json();
-    console.log(json);
-    setListOfRestaurants(json?.data?.cards);
- 
- 
-  };
+   
+    const fetchData = async() => {
+      const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9979891&lng=77.55048529999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+      const json = await data.json();
+      console.log(json);
+      setListOfRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+      
+   
+ };
+
+    if(listOfRestaurants.length === 0) {
+      return <h1>Loading...</h1>;
+    }
+
 
   
 
 
-  
     return (
           <div className="Body">
              <div className="filter">
@@ -45,7 +51,7 @@ const Body = () => {
              <div className="res-container">
                {
                  listOfRestaurants.map((restaurant) => (
-                    <RestaurantCard resData={restaurant.info} key={restaurant.info.id} />
+                    <RestaurantCard resData={restaurant?.info} key={restaurant?.info?.id} />
                  )
               
                 )
